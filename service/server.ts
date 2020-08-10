72b3dbc0ad1204099e5c6060a531df1f815072e2
Abduljalil, Timur <Timur.Abduljalil@LibertyMutual.com>
2020-08-10 19:57:16 -0400

    Add a basic service implementation

---
+++ service/server.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT);
+console.log(`Running on http://${HOST}:${PORT}`);
