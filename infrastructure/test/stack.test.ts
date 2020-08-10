fd9ace9e1f09a9937d1576f1c998fe3c5b8b6bd5
Abduljalil, Timur <Timur.Abduljalil@LibertyMutual.com>
2020-08-10 19:14:03 -0400

    Update dependencies and fix set up

--- infrastructure/test/stack.test.ts
+++ infrastructure/test/stack.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import { MyStack } from '../lib/stack';
+import { MyStack } from '../stack';
 
 test('Empty Stack', () => {
   const app = new cdk.App();
