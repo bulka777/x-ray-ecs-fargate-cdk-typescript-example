d3cb519c2d57e07f65e7ee6ba2a47528f2b06267
Abduljalil, Timur <Timur.Abduljalil@LibertyMutual.com>
2020-08-10 20:19:21 -0400

    Add cluster stack

--- infrastructure/app.ts
+++ infrastructure/app.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { EcrStack } from './ecr.stack';
+import { ClusterStack } from './cluster.stack';
 
 const app = new cdk.App();
-new EcrStack(app, 'MyStack', { env: { region: 'us-east-2', account: '776387660326' } });
+new EcrStack(app, 'EcrStack', { env: { region: 'us-east-2', account: '776387660326' } });
+new ClusterStack(app, 'ClusterStack', { env: { region: 'us-east-2', account: '776387660326' } });
---
+++ infrastructure/cluster.stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from '@aws-cdk/core';
+import * as ecs from '@aws-cdk/aws-ecs';
+
+export class ClusterStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new ecs.Cluster(this, 'Cluster');
+  }
+}
