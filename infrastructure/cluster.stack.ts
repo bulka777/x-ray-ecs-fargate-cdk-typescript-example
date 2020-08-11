392e231214d49ac8643237af8a94c6b1c70ea4d9
Abduljalil, Timur <Timur.Abduljalil@LibertyMutual.com>
2020-08-10 20:29:24 -0400

    Adjust the resource names

--- infrastructure/cluster.stack.ts
+++ infrastructure/cluster.stack.ts
@@ -5,6 +5,6 @@ export class ClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new ecs.Cluster(this, 'Cluster');
+    new ecs.Cluster(this, 'Cluster', { clusterName: 'example-cluster' });
   }
 }
--- infrastructure/ecr.stack.ts
+++ infrastructure/ecr.stack.ts
@@ -5,7 +5,7 @@ export class EcrStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const repository = new ecs.Repository(this, 'Repository');
+    const repository = new ecs.Repository(this, 'Repository', { repositoryName: 'example-ecr-repository' });
 
     repository.addLifecycleRule({ maxImageAge: cdk.Duration.days(30) });
   }
