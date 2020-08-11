5710b6fc2a57195b2f82f26493c5d21798168e0d
Abduljalil, Timur <Timur.Abduljalil@LibertyMutual.com>
2020-08-10 21:29:45 -0400

    Change to fargate service

--- infrastructure/app.ts
+++ infrastructure/app.ts
@@ -6,4 +6,4 @@ import { FargateServiceStack } from './fargate-service.stack';
 
 const app = new cdk.App();
 new EcrStack(app, 'EcrStack', { env: { region: 'us-east-2', account: '776387660326' } });
-new FargateServiceStack(app, 'FargateApp', { env: { region: 'us-east-2', account: '776387660326' } });
+new FargateServiceStack(app, 'FargateServiceStack', { env: { region: 'us-east-2', account: '776387660326' } });
--- infrastructure/fargate-service.stack.ts
+++ infrastructure/fargate-service.stack.ts
@@ -1,15 +1,33 @@
 import * as cdk from '@aws-cdk/core';
 import * as ecs from '@aws-cdk/aws-ecs';
+import * as iam from '@aws-cdk/aws-iam';
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 
 export class FargateServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const taskRole = new iam.Role(this, 'TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+    taskRole.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
+    });
+    const executionRole = new iam.Role(this, 'TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+    executionRole.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
+    });
+
+    const taskDefinition = new ecs.TaskDefinition(this, 'TaskDefinition', {
+      taskRole,
+      executionRole,
+      compatibility: ecs.Compatibility.FARGATE,
+    });
+
     new ApplicationLoadBalancedFargateService(this, 'ExampleService', {
-      taskImageOptions: {
-        image: ecs.ContainerImage.fromRegistry('776387660326.dkr.ecr.us-east-2.amazonaws.com/x-ray-example-ecs-app:latest'),
-      },
+      taskDefinition,
     });
   }
 }
